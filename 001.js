5b55de27cda11a9fc629ec28e8abc7dcb2cd7fe1
Vishwas <vishwasnigam03@gmail.com>
2022-09-24 16:18:57 +0530

    Second Commit

--- 001.js
+++ 001.js
@@ -4,7 +4,10 @@ console.log(a)
 a = "Behen ki laudi"
 console.log(a)
 // var was used before ES6 and we should avoid it
-
+let b = 89
+console.log(b)
+b = "Shrisha Padodi"
+console.log(b)
 // const is used to declare constants that are globally scoped
 
 // let is used to declare block scoped variables. it can only be updated but not redeclared
--- 003_exp_oper.js
+++
@@ -1,5 +0,0 @@
-let a = 5
-let b = 8
-let c = Number
-c = a + b
-console.log(c)
