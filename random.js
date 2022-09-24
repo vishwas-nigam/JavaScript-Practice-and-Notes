5f8384a5ab21f2d2e159c94eefd6db5391cea109
Vishwas <vishwasnigam03@gmail.com>
2022-09-24 16:22:50 +0530

    Random Number Generator

---
+++ random.js
@@ -0,0 +1,9 @@
+// input from the user
+const min = 0
+const max = 10000000000
+
+// generating a random number
+const a = Math.floor(Math.random() * (max - min + 1)) + min;
+
+// display a random number
+console.log(`Random value between ${min} and ${max} is ${a}`);
